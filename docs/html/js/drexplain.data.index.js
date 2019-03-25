fe013c6121d137480c9fde87beb81c27231f6f7b
jkennedy-usgs <jkennedy@usgs.gov>
2019-03-25 12:42:30 -0700

    Reorganized help

---
+++ docs/html/js/drexplain.data.index.js
@@ -0,0 +1,13 @@
+DR_EXPLAIN.namespace( 'DR_EXPLAIN.data_index' );
+DR_EXPLAIN.data_index = {
+
+	// index
+	DREX_NODE_KEYWORDS: [16,12,21,8,9,10,6,11,12,22,24,23,16,4,20,3,9,10,2,7,8,1,14,13,15,5,19,18,17,17],
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,2,6,6,6,6,7,7,7,8,8,8,8,10,12,12,12,13,13,13,13,13,15,16,16,16,16,16,16,16,16,16,16,16,16,18,18,18,18,19,21,22,22,25,25,25,27,27,27,27,27,27,27,27,29,29], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,2,6,6,6,6,7,7,7,8,8,8,8,10,12,12,12,13,13,13,13,13,15,16,16,16,16,16,16,16,16,16,16,16,16,18,18,18,18,19,21,22,22,25,25,25,27,27,27,27,27,27,27,27,29,29,30], //length:= drex.nodes_count,
+
+	DREX_KEYWORD_NAMES: ["<NEW KEYWORD>","Importing data","Stations","Network adjustment","Absolute gravity data","Formats","Installing GSadjust","Loops","Drift correction","Vertical gradients","Gradients, vertical","Workflow","Data","Duplicating","Deleting","Moving to new loop","Data plot","Adjustment options","Importing","Relative-gravity data","Absolute-gravity data","Drift correction","Loading data","Appending data to a new survey","Appending data to a new loop"],
+	DREX_KEYWORD_CHILD_START: [1,13,13,17,18,19,21,21,22,22,22,22,22,25,25,25,25,25,25,25,25,25,25,25,25],
+	DREX_KEYWORD_CHILD_END: [13,13,17,18,19,21,21,22,22,22,22,22,25,25,25,25,25,25,25,25,25,25,25,25,25]
+	
+};

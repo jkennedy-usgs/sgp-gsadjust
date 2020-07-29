Vertical gradient fixes, improved summary output

gsadjust/GSadjust.py
@@ -260,11 +260,13 @@ class MainProg(QtWidgets.QMainWindow):
         # Right-click tree view context menu
         self.menus.mnDeleteSurvey = self.menus.create_action('Delete survey', slot=self.delete_survey)
         self.menus.mnDeleteLoop = self.menus.create_action('Delete loop', slot=self.delete_loop)
-        self.menus.mnLoopProperties = self.menus.create_action('Loop properties', slot=self.properties_loop)
-        self.menus.mnStationRename = self.menus.create_action('Rename station', slot=self.rename_station)
-        self.menus.mnStationDelete = self.menus.create_action('Delete station(s)', slot=self.delete_station)
+        self.menus.mnLoopProperties = self.menus.create_action('Loop properties...', slot=self.properties_loop)
+        self.menus.mnRename = self.menus.create_action('Rename', slot=self.rename)
+        self.menus.mnDeleteStation = self.menus.create_action('Delete station(s)', slot=self.delete_station)
         self.menus.mnStationDuplicate = self.menus.create_action('Duplicate station', slot=self.duplicate_station)
         self.menus.mnDataNewLoop = self.menus.create_action('Move stations to new loop', slot=self.new_loop)
+        self.menus.mnVerticalGradientWriteAction = self.menus.create_action("Write vertical gradient file...",
+                                                                    slot=self.vertical_gradient_write)
         self.menus.mnLoopAnimate = self.menus.create_action('Animate loop', slot=self.animate_loop)
 
         # self.resize(600,800)
@@ -435,7 +437,7 @@ class MainProg(QtWidgets.QMainWindow):
         if fname:
             self.settings.setValue('current_dir', os.path.dirname(fname))
             if fname[-2:] == '.p' or fname[-4:] == '.gsa':
-                self.msg = show_message('Please use "Open workspace... " to load a .p or .gsa file', 'File load error')
+                self.msg = show_message('Please use "Open workspace... " to load a .gsa file', 'File load error')
                 return
             self.open_raw_data(fname, open_type)
 
@@ -685,7 +687,7 @@ class MainProg(QtWidgets.QMainWindow):
 
         if not fname or fname[-2:] != '.p':
             self.msg = show_message(
-                'Saved workspaces should have a .p or .gsa extension. '
+                'Saved workspaces should have a .gsa extension. '
                 'Please use "Open workspace..." to load a .gsa file, or '
                 '"Open raw...data" to load a data file.', 'File load error'
             )
@@ -714,8 +716,7 @@ class MainProg(QtWidgets.QMainWindow):
             self.msg = show_message(
                 'Saved workspaces should have a .gsa extension. '
                 'Please use "Open raw...data" to load a data file'
-                ' or "Open workspace (.p format)" to open a workspace'
-                ' with .p extension.', 'File load error'
+                , 'File load error'
             )
             return
 
@@ -1233,26 +1234,61 @@ class MainProg(QtWidgets.QMainWindow):
         Dialog that queries user for the distance over which vertical gradient is measured.
         """
         interval = VerticalGradientDialog(self.vertical_gradient_interval)
-        self.vertical_gradient_interval = interval
+        if interval.ok:
+            self.vertical_gradient_interval = interval.text
+
 
     def vertical_gradient_write(self):
         """
         Writes a .grd file with two values: gradient and standard deviation. Only works when Roman drift method
         is used.
         """
-        deltamodel = self.tab_drift.delta_view.model()
-        if deltamodel.rowCount() == 1:
-            stationname = self.obsTreeModel.itemFromIndex(self.index_current_loop).child(0).name
+        # Already verified that only a single loop is selected
+        # Want to get the selected loop, not the self.index_current_loop (former is highlighted, latter is bold)
+        current_loop_index = self.gui_data_treeview.selectedIndexes()[0]
+        current_loop = self.obsTreeModel.itemFromIndex(current_loop_index)
+        deltamodel = current_loop.delta_model
+        n_stations = current_loop.n_unique_stations()
+
+        dg, sd = None, None
+        if n_stations == 2:
+            stationname = self.obsTreeModel.itemFromIndex(self.index_current_loop).child(0).station_name
             defaultfile = os.path.join(self.settings.value('current_dir'), stationname + '.grd')
-            file_name, _ = QtWidgets.QFileDialog.getSaveFileName(None, 'Vertical gradient file to write',
-                                                                 defaultfile)
-            if file_name:
-                delta = deltamodel.data(deltamodel.index(0, 0), role=QtCore.Qt.UserRole)
-                with open(file_name, 'w') as fid:
-                    fid.write('{:0.2f}'.format(-1 * delta.dg / self.vertical_gradient_interval))
-                    fid.write(' +/- {:0.2f}'.format(delta.sd / self.vertical_gradient_interval))
+            try:
+                if current_loop.drift_method == 'roman':
+                    filename, _ = QtWidgets.QFileDialog.getSaveFileName(None, 'Vertical gradient file to write',
+                                                                         defaultfile)
+                    if filename:
+                        delta = deltamodel.data(deltamodel.index(0, 0), role=QtCore.Qt.UserRole)
+                        dg = delta.dg()
+                        sd = delta.sd
+                elif current_loop.drift_method == 'none' or current_loop.drift_method == 'continuous':
+                    filename, _ = QtWidgets.QFileDialog.getSaveFileName(None, 'Vertical gradient file to write',
+                                                                         defaultfile)
+                    if filename:
+                        dg_list, sd_list = [], []
+                        for i in range(deltamodel.rowCount()):
+                            idx = deltamodel.index(i,0)
+                            delta = deltamodel.data(idx, QtCore.Qt.UserRole)
+                            dg_list.append(delta.dg())
+                            sd_list.append(delta.sd)
+                        dg = np.mean(dg_list)
+                        sd = np.mean(sd_list)
+                else:
+                    self.msg = show_message('When writing a vertical gradient the drift correction method must be '
+                                            '"None", "Roman", or "Continuous"')
+                if dg:
+                    with open(filename, 'w') as fid:
+                        fid.write('{:0.2f}'.format(-1 * dg / self.vertical_gradient_interval))
+                        fid.write(' +/- {:0.2f}'.format(sd / self.vertical_gradient_interval))
+            except:
+                self.msg = show_message('Error writing gradient file (try visiting the Drift tab then re-running this '
+                                        'command).', 'Vertical gradient error')
         else:
-            self.msg = show_message("Incorrect number of delta-g's (should be 1; try the Roman method)",
+            self.msg = show_message('Writing a vertical gradient file requires that the respective loop has only '
+                                    'two stations. The gradient interval is set '
+                                    'by the "Vertical gradient interval..." menu command (instrument height in the '
+                                    'meter file is ignored).',
                                     "Vertical gradient error")
 
     def add_tare(self):
@@ -1348,13 +1384,11 @@ class MainProg(QtWidgets.QMainWindow):
             # for loop in self.loops:
 
             for loop in loops:
-                loop.oper = loop_options.operator_edit.text()
-                loop.meter = loop_options.meter_edit.text()
                 loop.comment = loop_options.comment_edit.toPlainText()
                 for i in range(loop.rowCount()):
                     obstreestation = loop.child(i)
-                    obstreestation.meter = [loop.meter] * len(obstreestation.meter)
-                    obstreestation.oper = [loop.oper] * len(obstreestation.oper)
+                    obstreestation.meter = [loop_options.meter_edit.text()] * len(obstreestation.meter)
+                    obstreestation.oper = [loop_options.operator_edit.text()] * len(obstreestation.oper)
             self.set_window_title_asterisk()
         self.update_data_tab()
 
@@ -1441,13 +1475,13 @@ class MainProg(QtWidgets.QMainWindow):
         self.set_window_title_asterisk()
         self.update_menus()
 
-    def rename_station(self):
+    def rename(self):
         """
         Rename station; same as F2.
         """
         indexes = self.gui_data_treeview.selectedIndexes()
-        if len(indexes) > 3 or len(indexes) == 0:
-            return
+        # if len(indexes) > 3 or len(indexes) == 0:
+        #     return
         # Because each tree item has three columns, len(indexes) equals the number of items selected * 3. The next
         # line takes every 3rd index.
         index = indexes[0]
@@ -1712,7 +1746,7 @@ class MainProg(QtWidgets.QMainWindow):
                     new_obstreeloop.appendRow([new_obstreestation,
                                                QtGui.QStandardItem('a'),
                                                QtGui.QStandardItem('a')])
-            copy_fields = ['meter', 'oper', 'source']
+            copy_fields = ['source']
             for field in copy_fields:
                 try:
                     setattr(new_obstreeloop, field, getattr(obstreeloop, field))
@@ -1736,20 +1770,22 @@ class MainProg(QtWidgets.QMainWindow):
         Right-click context menu on tree view
         """
         self.gui_data_treeview_popup_menu = QtWidgets.QMenu("Menu", self)
+        self.gui_data_treeview_popup_menu.addAction(self.menus.mnRename)
+        self.gui_data_treeview_popup_menu.addSeparator()
         self.gui_data_treeview_popup_menu.addAction(self.menus.mnDeleteSurvey)
+        self.gui_data_treeview_popup_menu.addSeparator()
         self.gui_data_treeview_popup_menu.addAction(self.menus.mnDeleteLoop)
-        self.gui_data_treeview_popup_menu.addSeparator()
         self.gui_data_treeview_popup_menu.addAction(self.menus.mnLoopProperties)
+        self.gui_data_treeview_popup_menu.addAction(self.menus.mnVerticalGradientWriteAction)
+        self.gui_data_treeview_popup_menu.addAction(self.menus.mnLoopAnimate)
         self.gui_data_treeview_popup_menu.addSeparator()
-        self.gui_data_treeview_popup_menu.addAction(self.menus.mnStationRename)
-        self.gui_data_treeview_popup_menu.addAction(self.menus.mnStationDelete)
+        self.gui_data_treeview_popup_menu.addAction(self.menus.mnDeleteStation)
         self.gui_data_treeview_popup_menu.addAction(self.menus.mnStationDuplicate)
         self.gui_data_treeview_popup_menu.addAction(self.menus.mnDataNewLoop)
-        self.gui_data_treeview_popup_menu.addSeparator()
-        self.gui_data_treeview_popup_menu.addAction(self.menus.mnLoopAnimate)
         # enable as appropriate
         indexes = self.gui_data_treeview.selectedIndexes()
-        if indexes:
+
+        if len(indexes) == 3:  # One item is selected
             index = indexes[0]
             item = index.model().itemFromIndex(index)
             if type(item) is ObsTreeStation:
@@ -1758,8 +1794,16 @@ class MainProg(QtWidgets.QMainWindow):
                 self.menus.set_state(MENU_STATE.OBS_TREE_LOOP)
             elif type(item) is ObsTreeSurvey:
                 self.menus.set_state(MENU_STATE.OBS_TREE_SURVEY)
+        elif len(indexes) > 3:
+            index_types = [type(index.model().itemFromIndex(index)) for index in indexes[0::3]]
+            if all([it == ObsTreeStation for it in index_types]):
+                self.menus.set_state(MENU_STATE.MULTIPLE_STATION)
+            else:
+                self.menus.set_state(MENU_STATE.UNENABLE_ALL)
+        else:
+            self.menus.set_state(MENU_STATE.UNENABLE_ALL)
+        self.gui_data_treeview_popup_menu.exec_(self.gui_data_treeview.mapToGlobal(point))
 
-            self.gui_data_treeview_popup_menu.exec_(self.gui_data_treeview.mapToGlobal(point))
 
     def adjust_network(self, how_many='all'):
         """

gsadjust/data_analysis.py
@@ -47,13 +47,12 @@ def numpy_inversion(adjustment, results_model, output_root_dir, write_out_files=
 
     n_meters = adjustment.n_meters
 
-    # number of unknowns:
     # TODO: temperature drift function
     drift_time = 0  # adjustment.adjustmentoptions.drift_time
-    if adjustment.adjustmentoptions.use_model_temp:
-        drift_temp = adjustment.adjustmentoptions.model_temp_degree
-    else:
-        drift_temp = 0
+    # if adjustment.adjustmentoptions.use_model_temp:
+    #     drift_temp = adjustment.adjustmentoptions.model_temp_degree
+    # else:
+    #     drift_temp = 0
 
     ndrift = adjustment.ndrift
 
@@ -63,7 +62,7 @@ def numpy_inversion(adjustment, results_model, output_root_dir, write_out_files=
 
     # Initialize least squares matrices
     # number of unknowns                                                                   #
-    nb_x = len(sta_dic_ls) + ndrift + drift_temp * nloops + n_meters
+    nb_x = len(sta_dic_ls) + ndrift + n_meters
     adjustment.adjustmentresults.n_unknowns = nb_x
     # model matrix:
     A = np.zeros((n_rel_obs + n_abs_obs, nb_x))

gsadjust/data_objects.py
@@ -299,22 +299,23 @@ class Delta:
             return self.sta2_t() - self.sta1_t()
 
     def __str__(self):
-        if self.checked == 2:
-            in_use = '1'
-        else:
-            in_use = '0'
+        # if self.checked == 2:
+        #     in_use = '1'
+        # else:
+        #     in_use = '0'
         # Rarely a station time will be -999 (if all samples are unchecked)
-        if self.sta1_t() != -999:
-            delta_time = num2date(self.sta1_t()).strftime('%Y-%m-%d %H:%M:%S')
-        else:
-            delta_time = '-999'
-        return_str = '{} {} {} {} {:0.3f} {:0.3f} {:0.3f}'.format(in_use,
+        # if self.sta1_t() != -999:
+        #     delta_time = num2date(self.sta1_t()).strftime('%Y-%m-%d %H:%M:%S')
+        # else:
+        #     delta_time = '-999'
+        return_str = '{} {} {} {} {:0.3f} {:0.3f} {:0.3f} {:0.3f}'.format(int(self.checked/2),
                                                                   self.sta1,
                                                                   self.sta2,
-                                                                  delta_time,
+                                                                  self.time_string(),
                                                                   self.dg(),
                                                                   self.sd,
-                                                                  self.sd_for_adjustment)
+                                                                  self.sd_for_adjustment,
+                                                                  self.residual)
         return return_str
 
     def time(self):
@@ -325,6 +326,12 @@ class Delta:
         elif type(self.station2) is list:
             t = [delta.sta1_t() for delta in self.station2]
             return np.mean(t)
+
+    def time_string(self):
+        try:
+            return num2date(self.time()).strftime('%Y-%m-%d %H:%M:%S')
+        except:
+            return '-999'
 
 
 ###############################################################################
@@ -389,10 +396,15 @@ class Datum:
         Override the built-in method 'print' when applied to such object
         """
         if self.checked == 2:
-            in_use = '1 '
+            in_use = '1'
         else:
-            in_use = '0 '
-        return in_use + self.station + ' ' + str(self.g) + ' ' + str(self.sd) + ' ' + self.date
+            in_use = '0'
+        return '{} {} {} {:.2f} {:.2f} {:.2f}'.format(in_use,
+                                                   self.station,
+                                                   self.date,
+                                                   self.g,
+                                                   self.sd,
+                                                   self.residual)
 
 
 ###############################################################################
@@ -558,8 +570,8 @@ class AdjustmentOptions:
     """
 
     def __init__(self):
-        self.use_model_temp = False
-        self.model_temp_degree = 0
+        # self.use_model_temp = False
+        # self.model_temp_degree = 0
         self.use_sigma_prefactor = False
         self.sigma_prefactor = 1.0
         self.use_sigma_postfactor = False
@@ -569,7 +581,6 @@ class AdjustmentOptions:
         self.use_sigma_min = False
         self.sigma_min = 3.0
         self.alpha = 0.05
-        self.woutfiles = False
         self.cal_coeff = False
         self.adj_type = 'gravnet'
         self.specify_cal_coeff = False
@@ -635,27 +646,28 @@ class Adjustment:
                 # text_out.append("Number of stations:                 {:d}\n".format(len(sta_dic_ls)))
                 # text_out.append("Number of loops:                    {:d}\n".format(nloops))
                 # text_out.append("Polynomial degree for time:         {:d}\n".format(drift_time))
-                text_out.append("Number of unknowns:                 {:d}\n".format(self.adjustmentresults.n_unknowns))
-                text_out.append("Number of relative observations:    {:d}\n".format(self.adjustmentresults.n_deltas))
-                text_out.append("Number of absolute observations:    {:d}\n".format(self.adjustmentresults.n_datums))
+                text_out.append("Number of unknowns:                  {:d}\n".format(self.adjustmentresults.n_unknowns))
+                text_out.append("Number of relative observations:     {:d}\n".format(self.adjustmentresults.n_deltas))
+                text_out.append("Number of absolute observations:     {:d}\n".format(self.adjustmentresults.n_datums))
                 text_out.append("Degrees of freedom (nobs-nunknowns): {:d}\n".format(self.dof))
-                text_out.append(
-                    "SD a posteriori:                    {:f}\n".format(
+                text_out.append("SD a posteriori:                     {:4f}\n".format(
                         float(np.sqrt(self.VtPV / self.dof))))
-                text_out.append(
-                    "chi square value:                   {:6.2f}\n".format(float(self.adjustmentresults.chi2)))
-                text_out.append(
-                    "critical chi square value:          {:6.2f}\n".format(float(self.adjustmentresults.chi2c)))
+                text_out.append("chi square value:                  {:6.2f}\n".format(float(
+                    self.adjustmentresults.chi2)))
+                text_out.append("critical chi square value:         {:6.2f}\n".format(float(
+                    self.adjustmentresults.chi2c)))
                 if float(self.adjustmentresults.chi2) < float(self.adjustmentresults.chi2c):
                     text_out.append("Chi-test accepted\n")
                 else:
                     text_out.append("Chi-test rejected\n")
 
-                text_out.append("Average standard deviation: {:.2f}\n".format(self.adjustmentresults.avg_stdev))
-                text_out.append("Maximum delta residual: {:.2f}\n".format(self.adjustmentresults.max_dg_residual))
-                text_out.append("Maximum absolute datum residual: {:.2f}\n".format(
+                text_out.append("Average standard deviation:          {:.2f}\n".format(
+                    self.adjustmentresults.avg_stdev))
+                text_out.append("Maximum delta residual:              {:.2f}\n".format(
+                    self.adjustmentresults.max_dg_residual))
+                text_out.append("Maximum absolute datum residual:     {:.2f}\n".format(
                     self.adjustmentresults.max_datum_residual))
-                text_out.append("Minimum absolute datum residual: {:.2f}\n".format(
+                text_out.append("Minimum absolute datum residual:     {:.2f}\n".format(
                     self.adjustmentresults.min_datum_residual))
                 if self.adjustmentoptions.cal_coeff:
                     for k, v in self.adjustmentresults.cal_dic.items():